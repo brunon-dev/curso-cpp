f5e6c0b80c2c7815cbcf82d0c205e58b3c1da450
Bruno Figueiredo Nogueira <bruno.nogueira@orama.corp>
2021-11-03 21:24:54 -0300

    Faz o primeiro código do curso

---
+++ adivinhacao/jogoadivinhacao.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "**************************************" << endl;
+    cout << "* Bem-vindos ao jogo de adivinhação! *" << endl;
+    cout << "**************************************" << endl;
+}
