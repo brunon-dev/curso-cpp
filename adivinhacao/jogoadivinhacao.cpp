6de57ce735f58079aeb6defe6489a2432ae6d4e7
Bruno Figueiredo Nogueira <bruno@brunon.com.br>
2021-11-05 23:41:18 -0300

    Coloca a leitura do chute, compara com uma constante de número secreto, valida e controla tentativas

--- adivinhacao/jogoadivinhacao.cpp
+++ adivinhacao/jogoadivinhacao.cpp
@@ -6,4 +6,38 @@ int main() {
     cout << "* Bem-vindos ao jogo de adivinhação! *" << endl;
     cout << "**************************************" << endl;
     
+    const int NUMERO_SECRETO = 42;
+    
+    bool nao_acertou = true;
+    int tentativas = 0;
+
+    while(nao_acertou) {
+        
+        tentativas++;
+
+        int chute;
+
+        cout << "Tentativa " << tentativas << endl;
+
+        cout << "Qual o seu chute? ";
+        cin >> chute;
+        cout << "O valor do seu chute é " << chute << endl;
+
+        bool acertou = chute == NUMERO_SECRETO;
+        bool maior = chute > NUMERO_SECRETO;
+
+        if (acertou) {
+            cout << "Parabéns! Você acertou o número secreto!" << endl;
+            nao_acertou = false;
+        }
+        else if (maior) {
+            cout << "Seu chute foi maior que o número secreto!" << endl;
+        }
+        else {
+            cout << "Seu chute foi menor que o número secreto!" << endl;
+        }
+    }
+
+    cout << "Fim de jogo!" << endl;
+    cout << "Você acertou o número secreto em " << tentativas << " tentativas!" << endl;
 }
