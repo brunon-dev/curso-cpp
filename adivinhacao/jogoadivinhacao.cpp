7bf5b379655f282bfca909b0e1909d9faa75ddce
Bruno Figueiredo Nogueira <bruno@brunon.com.br>
2021-11-06 22:26:34 -0300

    Define o valor do número secreto de forma aleatória

--- adivinhacao/jogoadivinhacao.cpp
+++ adivinhacao/jogoadivinhacao.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 #include <Windows.h>
 using namespace std;
 
@@ -31,7 +33,8 @@ int main() {
         break;
     }
     
-    const int NUMERO_SECRETO = 42;
+    srand(time(0));
+    int numero_secreto = rand() % 100;
     
     bool nao_acertou = true;
 
@@ -43,17 +46,17 @@ int main() {
 
         int chute;
 
-        cout << "Tentativa " << tentativas << endl;
+        cout << "Tentativa " << tentativas << " de " << numero_de_tentativas << endl;
 
         cout << "Qual o seu chute? ";
         cin >> chute;
 
-        double pontos_perdidos = abs(chute - NUMERO_SECRETO) / 2.0;        
+        double pontos_perdidos = abs(chute - numero_secreto) / 2.0;        
         pontos = pontos - pontos_perdidos;
 
         cout << "O valor do seu chute é " << chute << endl;
-        bool acertou = chute == NUMERO_SECRETO;
-        bool maior = chute > NUMERO_SECRETO;
+        bool acertou = chute == numero_secreto;
+        bool maior = chute > numero_secreto;
 
         if (acertou) {
             cout << "Parabéns! Você acertou o número secreto!" << endl;
@@ -70,7 +73,7 @@ int main() {
 
     cout << "Fim de jogo!" << endl;
     if (nao_acertou) {
-        cout << "Você perdeu! Tente novamente!" << endl;
+        cout << "Você perdeu! O número secreto era " << numero_secreto << ". Tente novamente!" << endl;
     } else {
         cout << "Você acertou o número secreto em " << tentativas << " tentativas!" << endl;
         cout.precision(2);
