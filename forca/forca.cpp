5b4cb0c998811079c47068deff6812800ba3fe16
Bruno Figueiredo Nogueira <bruno@brunon.com.br>
2021-11-07 12:07:13 -0300

    Cria jogo da forca, pegando o chute de char do usuário

---
+++ forca/forca.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main () {
+    string palavra_secreta = "MELANCIA";
+
+    cout << "A palavra secreta é " << palavra_secreta << endl;
+
+    bool nao_acertou = true;
+    bool nao_enforcou = true;
+
+    while (nao_acertou && nao_enforcou) {
+        char chute;
+        cin >> chute;
+
+        cout << "O seu chute foi " << chute << endl;
+    }
+}
